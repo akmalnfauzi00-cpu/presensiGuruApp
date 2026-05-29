56e07de0150c2c7022268e9f89fcd119191d1c1b
Akmal Nur Fauzi <akmalnfauzi00@gmail.com>
2026-05-29 20:58:20 +0700

    push

--- src/constants/config.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL = "http://192.168.18.4";
-
-export const CONFIG = {
-  BASE_URL,
-  API_PREFIX: "/presensi-admin/public/api",
-  WEB_PREFIX: "/presensi-admin/public",
-  TIMEOUT_MS: 15000,
-};
